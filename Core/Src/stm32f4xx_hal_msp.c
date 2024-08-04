549d55b33456c26331d6b5579083b6389642b0b8
马龙伟 <2544047213@qq.com>
2024-08-04 17:29:50 +0800

    使用 Cube FW 1.25.2

--- Core/Src/stm32f4xx_hal_msp.c
+++ Core/Src/stm32f4xx_hal_msp.c
@@ -1,3 +1,4 @@
+
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
@@ -112,6 +113,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
   /* USER CODE BEGIN USART1_MspInit 1 */
 
   /* USER CODE END USART1_MspInit 1 */
+
   }
 
 }
